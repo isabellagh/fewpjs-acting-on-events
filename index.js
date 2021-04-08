946f1bdcac2442745226e7aef727931f8b818ebb
isabellagh <isabellagh88@gmail.com>
2021-04-07 21:38:10 -0400

    commit tree 6aabd0fa892034e3f51baaed017b985a67a2b792

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+
+dodger.style.bottom = "100px";
+dodger.style.bottom = "0px";
+
+
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`
+    }
+  }
+
+  function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`
+    }
+  }
+
+  document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft()
+    }
+  })
+
+  document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight()
+    }
+  })
